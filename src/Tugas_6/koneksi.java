85fb0dafe8558491e45651fb49dae34bc32143bb
ArifDwiSyafutra <arifdwi286@gmail.com>
2017-04-06 16:37:05 +0800

    posttest 6 selesai

---
+++ src/Tugas_6/koneksi.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas_6;
+
+/**
+ *
+ * @author arif
+ */
+import com.mysql.jdbc.Connection;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class koneksi {
+    public static java.sql.Connection con;
+        public static java.sql.Connection getConnection(){
+            try{
+                con= DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/praktikum_visual","root","");
+                JOptionPane.showMessageDialog(null,"Koneksi Berhasil");
+            }catch(SQLException e){
+                JOptionPane.showMessageDialog(null, "Koneksi Gagal  :"+
+                        e.getMessage());
+            }
+            return con;
+        }
+}
